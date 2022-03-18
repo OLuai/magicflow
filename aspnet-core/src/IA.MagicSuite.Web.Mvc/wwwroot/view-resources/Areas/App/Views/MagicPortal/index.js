4927aceeb377acc2bd0c4aecea4b7a2b27783903
maraBOOT <doueameaud13@gmail.com>
2022-03-18 12:11:32 +0000

    1

--- aspnet-core/src/IA.MagicSuite.Web.Mvc/wwwroot/view-resources/Areas/App/Views/MagicPortal/index.js
+++ aspnet-core/src/IA.MagicSuite.Web.Mvc/wwwroot/view-resources/Areas/App/Views/MagicPortal/index.js
@@ -1,16 +1,7 @@
-﻿var GridStack = import('../../../../../view-resources/Areas/App/Views/_Bundles/gridstack.min.js');
-console.log(GridStack);
+﻿import('../../../../../view-resources/Areas/App/Views/_Bundles/gridstack.min.js');
+alert('done !');
 $(function () {
-    var items = [
-        { content: 'my first widget' }, // will default to location (0,0) and 1x1
-        { w: 2, content: 'another longer widget!' } // will be placed next at (1,0) and 2x1
-    ];
-    var grid = GridStack.init();
-    grid.load(items);
-    ////var grid = GridStack.init();
-
-
-   // grid.addWidget({ w: 2, content: 'item 1' });
+    
 
 })
 
