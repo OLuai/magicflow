61b31176ed11eaf20595da40e2558d5a1a8f8c3c
OLuai <ouragaluai@yahoo.com>
2022-03-21 17:50:06 +0000

    v1

--- aspnet-core/src/IA.MagicSuite.Web.Mvc/wwwroot/view-resources/Areas/App/Views/MagicWidget/iamWidget.js
+++ aspnet-core/src/IA.MagicSuite.Web.Mvc/wwwroot/view-resources/Areas/App/Views/MagicWidget/iamWidget.js
@@ -1,17 +1,18 @@
 ﻿var iamWidget = {
-    init: function (propertyGridId, editAttributesId, htmlEditorId, cssEditorId, jsEditorId, widgetPreviewId, runButtonId) {
+    init: function () {
         let v = iamWidget.variables;
         let edi = iamWidget.codeEditor;
 
-        v.runButtonId = runButtonId || "iamWidgetPreviewButton";
+        //Initialisation des principales variables pour le mode de creation d'un widget
+        v.runButtonId = "iamWidgetPreviewButton";
+        v.propertyGridId = "iamWidgetAttributesPropertyGrid";
+        v.editAttributesId = "iamWidgetSettingAttributes";
+        v.htmlEditorId = "iamWidgetCustomHTML";
+        v.cssEditorId = "iamWidgetCustomCSS";
+        v.jsEditorId = "iamWidgetCustomJS";
+        v.widgetPreviewId = "iamWidgetPreviewWindow";
 
-        v.propertyGridId = propertyGridId || "iamWidgetAttributesPropertyGrid";
-        v.editAttributesId = editAttributesId || "iamWidgetSettingAttributes";
-        v.htmlEditorId = htmlEditorId || "iamWidgetCustomHTML";
-        v.cssEditorId = cssEditorId || "iamWidgetCustomCSS";
-        v.jsEditorId = jsEditorId || "iamWidgetCustomJS";
-        v.widgetPreviewId = widgetPreviewId || "iamWidgetPreviewWindow";
-
+        //Initialisation des editeurs de texte
         edi.html = CodeMirror($(`#${v.htmlEditorId}`)[0], {
             lineNumbers: true,
             tabSize: 4,
@@ -28,34 +29,34 @@
         edi.js = CodeMirror($(`#${v.jsEditorId}`)[0], {
             lineNumbers: true,
             tabSize: 4,
-            value: "function myScript(){return 100;}\n",
+            value: "//La variable {widgetVariables} est un objet qui contient tous nos attributs\n",
             theme: "dracula",
             mode: "javascript"
         });
+
+        //Pour inhiber un petit bug
         $(`#${v.editAttributesId} , #${v.cssEditorId} , #${v.jsEditorId}`).toggleClass("show active");
 
-
+        //Generer la preview du widget
         $(`#${v.runButtonId}`).on('click', iamWidget.runPreview);
 
-        iamWidget.attributes.showAttributesGrid();
-          
-    },
-    runPreview: function () {
-        console.log('click');
-        let v = iamWidget.variables;
-        let edi = iamWidget.codeEditor;
+        //Créer le right panel pour les quickCreate/quickforms
+        iamShared.ui.rightPanelCreate(null, false, null);
+        //Creer un nouvel attribut
+        $(`#CreateNewWidgetAttributeButton`).on('click', function () {
+            iamShared.ui.rightPanelShow();
+            iamQF.createForm(iamWidget.iamQFObjects.attributeCreate, null, true, null, true, app, abp.services.app.magicData, true, true, null);
+        });
 
-        let htmlCode = edi.html.getValue();
-        let cssCode = `<style>${edi.css.getValue()}</style>`;
-        let jsCode = `<script>${edi.js.getValue()}</script>`;
+        //Lister les attributs dans le propertyGrid et le gridView
+        iamWidget.attributes.showAttributesListGrid();
+        iamWidget.attributes.showAttributesPropertyGrid();
 
-        let previewWindow = document.getElementById(v.widgetPreviewId).contentWindow.document;
-        previewWindow.open();
-        previewWindow.write(cssCode + htmlCode + jsCode);
-        previewWindow.close();
-
+        $('#btn-import').on('click', iamWidget.importWidget);
+        $('#btn-export').on('click', iamWidget.exportWidget);
 
     },
+
     variables: {
         propertyGridId: null,
         htmlEditorId: null,
@@ -63,12 +64,12 @@
         jsEditorId: null,
         widgetPreviewId: null,
         editAttributesId: null,
-        runButtonId:null
+        runButtonId: null
     },
     codeEditor: {
         css: null,
         html: null,
-        js:null,
+        js: null,
     },
     widget: {
         id: null,
@@ -76,65 +77,52 @@
         entityId: null,
         attributes: [
             {
-                name: "nom",
-                type: "string",
-                public: true,
+                Name: "nom",
+                Type: "String",
+                Public: true,
+                Category: "Attributes",
             },
         ],
+        attributesVal: {
+            "nom": null,
+        },
         widgetHTML: "",
         widgetCSS: "",
         widgetJS: "",
-        widgetJSON: {
+        
+        //render: function () {
 
-        },
-        option: function () { },
-        render: function () { },
-        onContentReady: function () { },
-        beforeContentReady: function () { },
+        //},
+        //onContentReady: function () { },
+        //beforeContentReady: function () { },
 
     },
     attributes: {
-        showAttributesGrid: function () {
+        showAttributesListGrid: function () {
             let attr = iamWidget.widget.attributes;
             $(`#widgetAttributesGridList`).dxDataGrid({
                 dataSource: attr,
                 hoverStateEnabled: true,
-                keyExpr: "name",
+                keyExpr: "Name",
                 columnFixing: { enabled: false },
                 columns: [
                     {
-                        dataField: "name",
+                        dataField: "Name",
                         caption: app.localize("Name"),
-                        width:150,
                     },
                     {
-                        dataField: "type",
+                        dataField: "Type",
                         caption: app.localize("Type"),
-                        width: 150,
                     },
                     {
-                        dataField: "public",
+                        dataField: "Public",
                         caption: app.localize("Public"),
                         dataType: "boolean",
-                        width: 150,
                     },
                     {
                         caption: app.localize("Actions"),
                         type: 'buttons',
                         buttons: [
-                            {
-                                hint: 'Edit',
-                                icon: 'edit',
-                                visible(e) {
-                                    return !e.row.isEditing;
-                                },
-                                disabled(e) {
-                                    return isChief(e.row.data.Position);
-                                },
-                                onClick(e) {
-                                    alert('edit');
-                                },
-                            },
                             {
                                 hint: 'Remove',
                                 icon: 'remove',
@@ -149,7 +137,6 @@
                                 },
                             }
                         ],
-                        width: 150,
                     },
                 ],
                 focusedRowEnabled: true,
@@ -165,6 +152,171 @@
             });
 
         },
+        showAttributesPropertyGrid: function () {
+            let v = iamWidget.variables;
+            let attr = iamWidget.widget.attributes;
+            let attrObj = iamWidget.widget.attributesVal;
+            iamQF.showObjectProperties(new Proxy(attrObj, iamWidget.attributes.propertyGridProxyValidatorHandler), "Attributes", attr, v.propertyGridId, true);
+        },
+        propertyGridProxyValidatorHandler: {
+            set: function (options, prop, value) {
+                options[prop] = value;
+                return true;
+            },
+        },
+        createAttribute: function (data) {
+            iamWidget.widget.attributes.push({ ...data, Category: "Attributes" });
+            iamWidget.widget.attributesVal[data.Name] = null;
+        },
+
+    },
+    iamQFObjects: {
+        attributeCreate: {
+            AutoCreateEditors: false,
+            Id: "iamQFFlowCreate",
+            Name: "Create New Attribute",
+            DisplayName: null,
+            PositionId: "rightpanel",
+            OnValidated: function (data) {
+                abp.ui.setBusy('body');
+                iamWidget.attributes.createAttribute(data);
+                abp.notify.info(app.localize('SavedSuccessfully'));
+                iamWidget.attributes.showAttributesListGrid();
+                iamWidget.attributes.showAttributesPropertyGrid();
+                abp.ui.clearBusy('body');
+            },
+            Data: null,
+            IgnoreStepsOrderNumber: false, //Ignore le numéro d'ordre attribué et ordonne par ordre de position dans le tableau des steps
+            IgnoreItemsOrderNumber: true,
+            Steps: [
+                {
+                    Id: "0001",
+                    Name: null,
+                    DisplayName: null,
+                    DenyBack: false,
+                    OrderNumber: 1
+                }
+            ],
+            Items: [
+                //{
+                //    Id: "item_Id",
+                //    StepId: "0001",
+                //    OrderNumber: null,
+                //    ColSpan: null,
+                //    CssClass: null,
+                //    DataField: "id",
+                //    DisplayName: "Id",
+                //    IsRequired: true,
+                //    ReadOnly: true,
+                //    EditorType: "dxTextBox",
+                //    Formula: create ? `IF({tenantId}=NULL;convertToPascalCase({name}); CONCATENATE("t";{tenantId};"_";convertToPascalCase({name})))` : null,
+                //},
+                {
+                    Id: "item_Name",
+                    StepId: "0001",
+                    OrderNumber: 1,
+                    DataField: "Name",
+                    DisplayName: "Name",
+                    IsRequired: true,
+                    EditorType: "dxTextBox",
+                    ValidationRules: [
+                        {
+                            type: 'pattern', //require, email,compare,range,stringLength
+                            pattern: '^[0-9A-Za-z_ ]+$',
+                            message: app.localize("InvalidDataInput")
+                        }
+                    ],
+                },
+                {
+                    Id: "item_FlowType",
+                    StepId: "0001",
+                    OrderNumber: null,
+                    DataField: "Type",
+                    DisplayName: "Type",//app.localize("AppType"),
+                    DefaultValue: "DUAL",
+                    IsRequired: false,
+                    EditorType: "dxTextBox",// "dxSelectBox",
+
+                    //ListDataSourceName: "FlowTypeSelect"
+                },
+                {
+                    Id: "item_IsPublic",
+                    StepId: "0001",
+                    OrderNumber: null,
+                    DataField: "Public",
+                    DisplayName: "Public",
+                    IsRequired: false,
+                    EditorType: "dxCheckBox",
+                    DefaultValue: true
+                },
+            ]
+        }
+    },
+    //la fonction renvoie une chaine html si selector est null
+    render: function (widget, selector) {
+
+        let codeCSS = `<style>${widget.widgetCSS}</style>`;
+        let codeHTML = ejs.render(widget.widgetHTML, widget.attributesVal);
+        let codeJS = `<script>var widgetVariables = ${JSON.stringify(widget.attributesVal)};${widget.widgetJS}</script>`.replaceAll("widgetVariables", `${widget.id}_widgetVariables`);
+        let code = `${codeCSS} ${codeHTML} ${codeJS}`;
+        if (!selector) return code;
+        return $(selector).append(code);
+    },
+    saveWidget: function () {
+        let edi = iamWidget.codeEditor;
+        let widget = iamWidget.widget;
+        widget.widgetCSS = edi.css.getValue();
+        widget.widgetHTML = edi.html.getValue();
+        widget.widgetJS = edi.js.getValue();
+    },
+    runPreview: function () {
+        let v = iamWidget.variables;
+        iamWidget.saveWidget();
+        let code = iamWidget.render(iamWidget.widget);
+
+        $(`#${v.widgetPreviewId}`).html("");
+        $(`#${v.widgetPreviewId}`).html(code);
+        //let previewWindow = document.getElementById(v.widgetPreviewId).contentWindow.document;
+
+        //previewWindow.open();
+        //previewWindow.write(code);
+        //previewWindow.close();
+    },
+    importWidget: function () {
+        let input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.json';
+
+        input.onchange = e => {
+            // getting a hold of the file reference
+            let file = e.target.files[0];
+
+            // setting up the reader
+            let reader = new FileReader();
+            reader.readAsText(file, 'UTF-8');
+
+            // here we tell the reader what to do when it's done reading...
+            reader.onload = readerEvent => {
+                var content = readerEvent.target.result; // this is the content!
+                let widget = JSON.parse(content);
+
+                iamWidget.widget = widget;
+
+                //Lister les attributs dans le propertyGrid et le gridView
+                iamWidget.attributes.showAttributesListGrid();
+                iamWidget.attributes.showAttributesPropertyGrid();
+
+                iamWidget.codeEditor.css.setValue(widget.widgetCSS);
+                iamWidget.codeEditor.html.setValue(widget.widgetHTML);
+                iamWidget.codeEditor.js.setValue(widget.widgetJS);
+                iamShared.messages.showSuccessMsg("Imported !")
+            }
+        }
+
+        input.click();
+    },
+    exportWidget: function () {
+        iamShared.files.stringToFileDownload("Widget_"+iamWidget.widget.name + ".json", JSON.stringify(iamWidget.widget));
     },
 };
 
